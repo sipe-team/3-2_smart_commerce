4d1304efc057e7426e52903b1f358ab808cd2c9b
woozi <kwj1270@naver.com>
2025-01-08 22:51:05 +0900

    chore(kwj1270): 미션 진행을 위한 기본 설정 추가

---
+++ common/src/main/java/com/smart/commerce/common/constant/DateTimeFormatterConstant.java
@@ -0,0 +1,8 @@
+package com.smart.commerce.common.constant;
+
+public class DateTimeFormatterConstant {
+    private DateTimeFormatterConstant() {
+    }
+
+    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+}

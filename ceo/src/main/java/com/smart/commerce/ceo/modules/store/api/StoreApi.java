40a31600cd8cf1243dd9a7ddde90faac1856feb8
woozi <kwj1270@naver.com>
2025-01-26 17:38:02 +0900

    feat(kwj1270): 가게 상태 조회 API 개발

--- ceo/src/main/java/com/smart/commerce/ceo/modules/store/api/StoreApi.java
+++ ceo/src/main/java/com/smart/commerce/ceo/modules/store/api/StoreApi.java
@@ -1,30 +1,30 @@
 package com.smart.commerce.ceo.modules.store.api;
 
-import com.smart.commerce.ceo.modules.store.api.dto.StoreRegisterRequest;
-import com.smart.commerce.ceo.modules.store.api.dto.StoreRegisterResponse;
-import com.smart.commerce.ceo.modules.store.api.dto.StoreUpdateStatusRequest;
-import com.smart.commerce.ceo.modules.store.api.dto.StoreUpdateStatusResponse;
+import com.smart.commerce.ceo.modules.store.api.dto.*;
+import com.smart.commerce.ceo.modules.store.application.StoreAssertOpenedUseCase;
 import com.smart.commerce.ceo.modules.store.application.StoreRegisterUseCase;
 import com.smart.commerce.ceo.modules.store.application.StoreUpdateStatusUseCase;
 import com.smart.commerce.ceo.modules.store.domain.Store;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.time.LocalTime;
 
 @RestController
 public class StoreApi {
 
     private final StoreRegisterUseCase storeRegisterUseCase;
     private final StoreUpdateStatusUseCase storeUpdateStatusUseCase;
+    private final StoreAssertOpenedUseCase storeAssertOpenedUseCase;
 
     public StoreApi(final StoreRegisterUseCase storeRegisterUseCase,
-                    final StoreUpdateStatusUseCase storeUpdateStatusUseCase) {
+                    final StoreUpdateStatusUseCase storeUpdateStatusUseCase,
+                    final StoreAssertOpenedUseCase storeAssertOpenedUseCase) {
         this.storeRegisterUseCase = storeRegisterUseCase;
         this.storeUpdateStatusUseCase = storeUpdateStatusUseCase;
+        this.storeAssertOpenedUseCase = storeAssertOpenedUseCase;
     }
 
     @PostMapping("/store")
@@ -46,5 +46,14 @@ public class StoreApi {
         final long ceoId = Long.valueOf(httpSession.getAttribute("ceo").toString());
         final Store store = storeUpdateStatusUseCase.updateStatus(request.toCommand(ceoId));
         return ResponseEntity.ok().body(new StoreUpdateStatusResponse(store.getId(), store.getStatus()));
+    }
+
+    @GetMapping("/store/status")
+    public ResponseEntity<StoreAssertStatusResponse> isOpened(
+            @RequestParam(value = "storeId") Long storeId
+    ) {
+        final LocalTime now = LocalTime.now();
+        final boolean isOpened = storeAssertOpenedUseCase.isOpened(storeId, now);
+        return ResponseEntity.ok().body(new StoreAssertStatusResponse(isOpened));
     }
 }
